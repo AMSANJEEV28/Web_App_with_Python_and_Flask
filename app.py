b4307614d092756bc32191395dabe285ef1ebb14
A M SANJEEV <amsanjeev28@gmail.com>
2023-06-02 22:31:05 +0530

    Routes

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-    
+from routes import *
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ routes.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
